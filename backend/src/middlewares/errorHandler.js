46242a1b8963ca06ec8b54f7701f78876ec72db2
Washimakhtar786 <washimwdn@gmail.com>
2026-07-24 13:26:14 +0530

    fix: use logger in error handler

--- backend/src/middlewares/errorHandler.js
+++ backend/src/middlewares/errorHandler.js
@@ -1,7 +1,7 @@
 import logger from "../utils/logger.js";
 
 export const errorHandler = (err, req, res, next) => {
-  logError(err);
+  logger.error(err);
 
   const status = err.status || 500;
   const message = err.message || "Something went wrong";
