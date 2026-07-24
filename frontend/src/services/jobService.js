a28adf2a320787329d7c2e84af83ed8a78aa2477
Washimakhtar786 <washimwdn@gmail.com>
2026-07-24 12:57:30 +0530

    fix: update jobs api endpoint

--- frontend/src/services/jobService.js
+++ frontend/src/services/jobService.js
@@ -1,7 +1,7 @@
 import API from "./api";
 
 export const fetchJobs = async () => {
-  const response = await API.get("/jobs");
+  const response = await API.get("/api/v1/jobs");
 
   return response.data.data;
 };
